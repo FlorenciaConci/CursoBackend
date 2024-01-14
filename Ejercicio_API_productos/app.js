0980d551cf0814d2e81e751510c71abac5efa855
FlorenciaConci <vm.ludovica@gmail.com>
2024-01-14 16:52:41 -0300

    api.libros

--- Ejercicio_API_productos/app.js
+++ Ejercicio_API_productos/app.js
@@ -3,16 +3,21 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('¡Hola, mundo!');
+});
+
+app.post('/productos', (req, res) => {
+    // Lógica para crear un nuevo producto
+    res.send('Producto creado correctamente');
+});
+
+app.get('/productos', (req, res) => {
+    const categoria = req.query.categoria;
+    // Aquí puedes usar el parámetro de categoría para realizar una búsqueda de productos en esa categoría
+    res.send(`Realizar búsqueda de productos en la categoría "${categoria}"`);
     });
+// Usa el puerto proporcionado por el entorno o elige 4000 como predeterminado
+const port = process.env.PORT || 4000;
 
-    app.post('/productos', (req, res) => {
-        // Lógica para crear un nuevo usuario
-        res.send('Producto creado correctamente');
-        });
-
-
-
-const port = 4000;
 app.listen(port, () => {
-console.log(`Servidor Express.js en funcionamiento en el puerto ${port}`);
+    console.log(`Servidor Express.js en funcionamiento en el puerto ${port}`);
 });
