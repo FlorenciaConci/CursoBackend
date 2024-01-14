f4eda56bc628cd8f332b85c234cf85069a300eeb
FlorenciaConci <vm.ludovica@gmail.com>
2024-01-14 16:42:00 -0300

    ultimos cambios

--- Ejercicio_API_productos/app.js
+++ Ejercicio_API_productos/app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo!');
+    });
+
+    app.post('/productos', (req, res) => {
+        // Lógica para crear un nuevo usuario
+        res.send('Producto creado correctamente');
+        });
+
+
+
+const port = 4000;
 app.listen(port, () => {
 console.log(`Servidor Express.js en funcionamiento en el puerto ${port}`);
 });
